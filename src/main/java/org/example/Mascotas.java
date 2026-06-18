63f04fcee39a033cce6dd86064fde50f3f0e2612
arenisca <vseron2000@gmail.com>
2026-06-18 01:12:39 -0400

    Feat: clase Mascotas similar a Productos, incluye métodos alimentar jugar limpiar y mejorarSalud

---
+++ src/main/java/org/example/Mascotas.java
@@ -0,0 +1,30 @@
+package org.example;
+
+// Esta clase será parecida a la de Producto en la tarea 1
+public abstract class Mascotas {
+    private int id;
+    private String nombre, tipo;
+    private int nivelHambre, nivelFelicidad, nivelSalud;
+
+    public Mascotas(int id, String nombre, String tipo){
+        this.id=id;
+        this.nombre=nombre;
+        this.tipo=tipo;
+        this.nivelFelicidad=50;
+        this.nivelHambre=50;
+        this.nivelSalud=100;
+    }
+
+
+
+    public abstract void alimentar();
+
+    public abstract void jugar();
+
+    public abstract void limpiar();
+
+    public abstract void mejorarSalud();
+    // de este metodo tengo dudas de si dependerá más de los 3 anteriores que es
+    // lo que comun mente se ve en simuladores de mascotas
+
+}
