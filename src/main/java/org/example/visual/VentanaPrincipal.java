a0d0053192beb28c7ed5cacf153177aba6acb980
arenisca <vseron2000@gmail.com>
2026-07-02 02:37:07 -0400

    Feat: creación de la clase VentanaPrincipal, por desarrollar

---
+++ src/main/java/org/example/visual/VentanaPrincipal.java
@@ -0,0 +1,13 @@
+package org.example.visual;
+
+import org.example.logica.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VentanaPrincipal {
+}
