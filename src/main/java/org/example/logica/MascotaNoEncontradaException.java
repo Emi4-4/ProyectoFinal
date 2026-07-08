da7fb4f3bbbbaffd12f33477a7866a0dc67cbef8
arenisca <vseron2000@gmail.com>
2026-07-07 23:21:28 -0400

    Feat: excepción en método de comprarMascotaDelProveedor

---
+++ src/main/java/org/example/logica/MascotaNoEncontradaException.java
@@ -0,0 +1,7 @@
+package org.example.logica;
+
+public class MascotaNoEncontradaException extends RuntimeException {
+    public MascotaNoEncontradaException(String message) {
+        super(message);
+    }
+}
